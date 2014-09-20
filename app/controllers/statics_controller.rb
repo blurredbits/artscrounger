12ae0ba3f14cde582f5ec14661715160b2569dd6
Blurredbits <mmorris00@mac.com>
2014-09-20 16:47:18 -0600

    Added HTML to public

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,3 +1,18 @@
 class StaticsController < ApplicationController
 
+  def index
+  end
+
+  def done
+  end
+
+  def friends
+  end
+
+  def mission
+  end
+
+  def mymap4
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   namespace :api, path: "", defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :missions,        only: [:index, :show, :create, :update]
