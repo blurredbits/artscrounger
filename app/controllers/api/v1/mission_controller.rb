bc1350f1e92ce43cfdba0da175aa356a183a6483
Blurredbits <mmorris00@mac.com>
2014-09-20 14:44:49 -0600

    Updating restful routes for missions and users

--- app/controllers/api/v1/mission_controller.rb
+++ app/controllers/api/v1/mission_controller.rb
@@ -1,24 +1,33 @@
 class API::V1::MissionController < ApplicationController
 
+  before_filter :get_mission, except: [:create, :index]
+
   def index
     @missions = Mission.all
     render json: @missions
   end
 
   def show
-    @mission = Mission.find(params[:id])
     render json: @mission
   end
 
   def create
-
+    @mission = Mission.new(mission_params)
+    save_or_render_error(@mission)
   end
 
   def update
-
+    @mission.attributes = mission_params
+    save_or_render_error(@mission)
   end
 
-  def destroy
+  private
 
+  def get_mission
+    @mission = Mission.find(params[:id])
+  end
+
+  def mission_params
+    params.require(:mission).permit(:name, :art_pieces_id, :user_id)
   end
 end
--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,5 +1,7 @@
 class API::V1::UserController < ApplicationController
 
+  before_filter :get_user, except: [:create, :index]
+  
   def index
     @users = User.all
     render json: @users
@@ -9,5 +11,25 @@ class API::V1::UserController < ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
-  
+
+  def create
+    @user = User.new(user_params)
+    save_or_render_error(@user)
+  end
+
+  def update
+    @user.attributes = user_params
+    save_or_render_error(@user)
+  end
+
+  private
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+
 end
